0eb0ea6b9c44576c74e01f4bfc37dbed35066243
Christian Würthner <c.wuerthner@me.com>
2019-06-16 10:51:52 +0200

    Add code

---
+++ index.js
@@ -0,0 +1,45 @@
+export.createBackupHandler = (projectId, bucket, bucketPath = 'firestore', firestoreInstance = '(default)') => {
+  return () => {
+    const moment = require('moment');
+    const request = require('request-promise')
+    const {
+      google
+    } = require('googleapis');
+
+    const auth = await google.auth.getClient({
+      scopes: ['https://www.googleapis.com/auth/datastore']
+    });
+
+    auth.getAccessToken().then(accessTokenResponse => {
+      const accessToken = accessTokenResponse.token;
+
+      const headers = {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + accessToken
+      };
+
+      const timestamp = moment().format('YYYY-MM-DD HH:mm:ss')
+      let path = 'gs://' + `${bucket}/${bucketPath}/${firestoreInstance}/`.replace(/\/\//g, '/');
+      if (path.endsWith('/')) {
+        path += timestamp;
+      } else {
+        path += '/' + timestamp;
+      }
+
+      const url = `https://firestore.googleapis.com/v1beta1/projects/${projectId}/databases/${firestoreInstance}:exportDocuments`;
+      const body = {
+        outputUriPrefix: path
+      };
+
+      console.log(`Starting backup for firestore instace '${projectId}/${firestoreInstance}'`);
+
+      return request.post(url, {
+        json: body,
+        headers: headers
+      });
+    }).then(response => {
+      console.log(`Backup completed with path '${path}'`);
+    }).catch(e => {
+      console.error('Backup failed', e.error || e);
+    });
+}
